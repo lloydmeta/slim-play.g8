07103267882588ea5279aa30e7d7992e1f0e9110
Lloyd <lloydmeta@users.noreply.github.com>
2019-05-20 10:04:04 +0900

    Upgrade all the things (#6)

--- src/main/g8/app/wiring/AppLoader.scala
+++ src/main/g8/app/wiring/AppLoader.scala
@@ -9,25 +9,27 @@ import play.api.routing.sird._
 import scala.concurrent.Future
 
 class AppComponents(context: Context)
-  extends BuiltInComponentsFromContext(context)
-  with NoHttpFiltersComponents {
+    extends BuiltInComponentsFromContext(context)
+    with NoHttpFiltersComponents {
 
   val router: Router = Router.from {
 
     // Essentially copied verbatim from the SIRD example
-    case GET(p"/hello/\$to") => Action {
-      Ok(s"Hello \$to")
-    }
+    case GET(p"/hello/\$to") =>
+      Action {
+        Ok(s"Hello \$to")
+      }
 
     /*
      Use Action.async to return a Future result (sqrt can be intense :P)
      Note the use of double(num) to bind only numbers (built-in :)
-      */
-    case GET(p"/sqrt/\${double(num)}") => Action.async {
-      Future {
-        Ok(Math.sqrt(num).toString)
+     */
+    case GET(p"/sqrt/\${double(num)}") =>
+      Action.async {
+        Future {
+          Ok(Math.sqrt(num).toString)
+        }
       }
-    }
 
   }
 
--- src/main/g8/test/helpers/MyApplicationFactory.scala
+++ src/main/g8/test/helpers/MyApplicationFactory.scala
@@ -13,7 +13,8 @@ trait MyApplicationFactory extends FakeApplicationFactory {
 
   // Override this with something of your choosing that returns perhaps
   // a subclass of AppComponents with a specific component overriden
-  def buildComponents(context: Context): AppComponents = new AppComponents(context)
+  def buildComponents(context: Context): AppComponents =
+    new AppComponents(context)
 
   private[this] var _components: AppComponents = _
 
@@ -21,15 +22,8 @@ trait MyApplicationFactory extends FakeApplicationFactory {
   final def components: AppComponents = _components
 
   override def fakeApplication: Application = {
-    val env           = Environment.simple(new File("."))
-    val configuration = Configuration.load(env)
-    val context = ApplicationLoader.Context(
-      environment = env,
-      sourceMapper = None,
-      webCommands = new DefaultWebCommands(),
-      initialConfiguration = configuration,
-      lifecycle = new DefaultApplicationLifecycle()
-    )
+    val env = Environment.simple(new File("."))
+    val context = Context.create(env)
     val components = buildComponents(context)
     _components = components
     components.application
